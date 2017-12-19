ffd64db2345b3a99d35d0a592c054850640f03f7
thisisaaroland <aaron@mapzen.com>
2017-12-19 13:45:40 -0800

    fetch data on to a non-existent VOLUME and then what?

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 # https://blog.docker.com/2016/09/docker-golang/
 # https://blog.golang.org/docker
 
-# docker build -t wof-pip .
+# docker build -t wof-pip-server .
 
 FROM golang
 
 ADD . /go-whosonfirst-pip-v2
 
 RUN cd /go-whosonfirst-pip-v2; make bin
+
+VOLUME /usr/local/data
 
 EXPOSE 8080
 
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,17 @@
 PIP_SERVER="/go-whosonfirst-pip-v2/bin/wof-pip-server"
 ARGS=""
 
+CURL=`which curl`
+BUNZIP2=`which bunzip2`
+
 if [ "${HOST}" != "" ]
 then
     ARGS="${ARGS} -host ${HOST}"
+fi
+
+if [ "${EXTRAS}" != "" ]
+then
+    ARGS="${ARGS} -extras"
 fi
 
 if [ "${WWW}" != "" ]
@@ -21,13 +29,48 @@ then
     
 fi
 
-${PIP_SERVER} ${ARGS} -mode sqlite /go-whosonfirst-pip-v2/data/*.db
+if [ "${MODE}" != "" ]
+then
+    ARGS="${ARGS} -mode ${MODE}"
+fi
+
+for DB in $(echo ${SOURCES} | sed "s/,/ /g")
+do
+    REMOTE="https://whosonfirst.mapzen.com/sqlite/${DB}.db"
+    LOCAL="/usr/local/data/${DB}.db"
+
+    if [ ! -f ${LOCAL} ]
+    then
+	echo "fetch ${REMOTE}"
+    
+	${CURL} -s -o ${LOCAL}.bz2 ${REMOTE}.bz2
+
+	if [ $? -ne 0 ]
+	then
+	    echo "failed to fetch remote source"
+	    exit 0
+	fi
+	
+	${BUNZIP2} ${LOCAL}.bz2
+
+	if [ $? -ne 0 ]
+	then
+	   echo "failed to uncompress local source"
+	   exit 0
+	fi
+	   
+    fi
+
+    ARGS="${ARGS} ${LOCAL}"    
+done
+
+echo ${PIP_SERVER} ${ARGS}
+${PIP_SERVER} ${ARGS}
 
 if [ $? -ne 0 ]
 then
-   echo "command '${STATICD} ${ARGS}' failed"
+   echo "command '${PIP_SERVER} ${ARGS}' failed"
    exit 1
 fi
 
 exit 0
-

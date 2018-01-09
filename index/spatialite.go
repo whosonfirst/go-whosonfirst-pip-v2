079fac6291058e372f6e8b8aadcaaa5614e6fa93
thisisaaronland <whosonfirst@mapzen.com>
2018-01-09 15:28:07 -0800

    notes to self inre spatialite

--- index/spatialite.go
+++ index/spatialite.go
@@ -16,21 +16,33 @@ import (
 )
 
 type SpatialiteIndex struct {
-     Index
-     Logger *log.WOFLogger
-     cache cache.Cache
+	Index
+	Logger *log.WOFLogger
+	cache  cache.Cache
 }
 
 func NewSpatialiteIndex(c cache.Cache) (Index, error) {
 
 	logger := log.SimpleWOFLogger("index")
-	
-     i := SpatialiteIndex {
-       cache: c,
-       Logger: logger,
-     }
 
-     return &i, nil
+	i := SpatialiteIndex{
+		cache:  c,
+		Logger: logger,
+	}
+
+	/*
+
+		conn2 = sqlite3.connect('whole-earth-spatialite.db')
+		conn2.enable_load_extension(True)
+		conn2.execute("SELECT load_extension('/usr/local/lib/mod_spatialite.dylib');")
+		conn2.execute("SELECT InitSpatialMetaData()")
+		conn2.execute("CREATE TABLE whosonfirst (id INTEGER PRIMARY KEY, name TEXT, placetype INTEGER, properties TEXT)")
+		conn2.execute("SELECT AddGeometryColumn('whosonfirst', 'geom', 2154, 'GEOMETRY', 'XY')")
+		conn2.execute("SELECT CreateSpatialIndex('whosonfirst', 'geom')")
+
+	*/
+
+	return &i, nil
 }
 
 func (i *SpatialiteIndex) Cache() cache.Cache {
@@ -39,9 +51,46 @@ func (i *SpatialiteIndex) Cache() cache.Cache {
 
 func (i *SpatialiteIndex) IndexFeature(f geojson.Feature) error {
 
+	/*
+
+	    for d in iterate_geojson():
+	   sql = """
+	   INSERT INTO whosonfirst
+	       (id, name, placetype, properties, geom)
+	   VALUES
+	       (:id, :name, :placetype, :properties, GeomFromText(:geom, 2154))
+	   """
+	   params = {
+	       'id': d['id'],
+	       'name': d['name'],
+	       'placetype': PlaceType[d['placetype']].value,
+	       'properties': json.dumps(d['properties']),
+	       'geom': shape(d['geometry']).wkt,
+	   }
+	   c.execute(sql, params
+
+	*/
 }
 
 func GetIntersectsByCoord(geom.Coord, filter.Filter) (spr.StandardPlacesResults, error) {
+
+	/*
+
+	select
+	  id, placetype, name, length(geom), properties
+	from
+	  whosonfirst
+	where
+	  within(GeomFromText('POINT(' || :longitude || ' ' || :latitude || ')'), geom)
+	  and rowid in (
+	        SELECT pkid FROM idx_whosonfirst_geom
+	        where xmin < :longitude
+	        and xmax > :longitude
+	        and ymin < :latitude
+	        and ymax > :latitude)
+	order by placetype desc;
+
+	*/
 
 }
 

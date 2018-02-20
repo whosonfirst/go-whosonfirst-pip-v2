1d3e219a088699f5793b6fa9f04592e9bcb6be0d
straup <straup@gmail.com>
2018-02-19 21:00:19 -0800

    start plugging in index/spatialite.go code - untested

--- index/spatialite.go
+++ index/spatialite.go
@@ -57,33 +57,44 @@ func (i *SpatialiteIndex) IndexFeature(f geojson.Feature) error {
 	return nil
 }
 
-func GetIntersectsByCoord(geom.Coord, filter.Filter) (spr.StandardPlacesResults, error) {
+func (i *SpatialiteIndex) GetIntersectsByCoord(coord geom.Coord, f filter.Filter) (spr.StandardPlacesResults, error) {
 
-	/*
+	db := i.database
 
-		select
-		  id, placetype, name, length(geom), properties
-		from
-		  whosonfirst
-		where
-		  within(GeomFromText('POINT(' || :longitude || ' ' || :latitude || ')'), geom)
-		  and rowid in (
-		        SELECT pkid FROM idx_whosonfirst_geom
-		        where xmin < :longitude
-		        and xmax > :longitude
-		        and ymin < :latitude
-		        and ymax > :latitude)
-		order by placetype desc;
+	conn, err := db.Conn()
 
-	*/
+	if err != nil {
+		return nil, err
+	}
+
+	lat := coord.Y
+	lon := coord.X
+
+	// spatialite> select name from states where within(GeomFromText('POINT(-97.74342 30.26771)'),states.Geometry);
+
+	q := `SELECT id FROM geometries WHERE WITHIN(GeomFromText('POINT(? ?)'), geom) AND rowid IN (
+		SELECT pkid FROM idx_whosonfirst_geom
+		        where xmin < ?
+		        and xmax > ?
+		        and ymin < ?
+		        and ymax > ?
+        )`
+
+	rows, err := conn.Query(q, lon, lat, lon, lon, lat, lat)
+
+	if err != nil {
+		return nil, err
+	}
+
+	i.Logger.Status("%v", rows)
 
 	return nil, errors.New("PLEASE WRITE ME")
 }
 
-func GetCandidatesByCoord(geom.Coord) (*pip.GeoJSONFeatureCollection, error) {
+func (i *SpatialiteIndex) GetCandidatesByCoord(coord geom.Coord) (*pip.GeoJSONFeatureCollection, error) {
 	return nil, errors.New("PLEASE WRITE ME")
 }
 
-func GetIntersectsByPath(geom.Path, filter.Filter) ([]spr.StandardPlacesResults, error) {
+func (i *SpatialiteIndex) GetIntersectsByPath(path geom.Path, f filter.Filter) ([]spr.StandardPlacesResults, error) {
 	return nil, errors.New("PLEASE WRITE ME")
 }
